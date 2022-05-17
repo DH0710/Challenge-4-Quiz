9a006a9654f143ab19956c1d282581016d5249f1
blur <damienhebert710@gmail.com>
2022-05-17 18:56:57 -0500

    new changes'

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-var startQuiz = document.querySelector("start-btn")
-var nextButton = document.querySelector("next-btn")
-var questionContainerElement = document.querySelector("question-container")
-var questionElement = document.querySelector("question")
+const startQuiz = document.querySelector("start-btn")
+const nextButton = document.querySelector("next-btn")
+const questionContainerElement = document.querySelector("question-container")
+const questionElement = document.querySelector("question")
 
 
 
