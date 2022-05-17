a9760ca0bdf0075b39ef59e9bc4e5d7e91a1c7f9
blur <damienhebert710@gmail.com>
2022-05-17 18:35:45 -0500

    more changes

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Challenge-4-Quiz
 
-I created a Quiz revolving around Spiderman, I was able to have each question randomized to where every question is different. 
-The start and next buttons work effectively to where to the quiz is able to move on to the next question. 
-I hope your spiderman knowledge is up to par!
+Created a Javascript Web Application. 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,7 @@
-var startButton = document.querySelector("start-btn")
+var startQuiz = document.querySelector("start-btn")
 var nextButton = document.querySelector("next-btn")
 var questionContainerElement = document.querySelector("question-container")
-
 var questionElement = document.querySelector("question")
-var answerButtonsElement = document.querySelector("answer-buttons")
 
 
 
