d74ac528829b1776c0aa1a52b573d1d6597cf0f5
blur <damienhebert710@gmail.com>
2022-05-17 19:24:28 -0500

    started flow of functions in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,22 @@
-const startQuiz = document.querySelector("start-btn")
+const startButton = document.querySelector("start-btn")
 const nextButton = document.querySelector("next-btn")
 const questionContainerElement = document.querySelector("question-container")
 const questionElement = document.querySelector("question")
 
 
 
+function startQuiz() {
 
+}
+
+function selectOption () {
+
+}
+
+function nextQuestion() {
+
+    
+}
 
 
 
