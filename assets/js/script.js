58e79a30402c66b200f3e6746aded01ae424cf34
blur <damienhebert710@gmail.com>
2022-05-17 23:11:09 -0500

    updated changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,63 +1,43 @@
 const startButton = document.querySelector(".start-btn")
 const nextButton = document.querySelector(".next-btn")
 const prevButton = document.querySelector(".prev-btn")
-const questionIndex = 0;
+const questionArr = 0;
 
 
-// questions for quiz
+// questions array for quiz
 const questionsArr = [
-    
+
     {
         question: 'Arrays in Javascript can be used to store:',
-        options: ['Booleans','Numbers and Strings','Other Arrays','All of the above',],
+        options: ['Booleans', 'Numbers and Strings', 'Other Arrays', 'All of the above',],
         answer: 'All of the above',
     },
     {
         question: 'Commonly used Data types DO NOT include?',
-        options: ['Strings','Boolean','Numbers','Alerts',],
+        options: ['Strings', 'Boolean', 'Numbers', 'Alerts',],
         answer: 'Boolean',
     },
     {
         question: 'String values must be enclosed within _____ when being assigned to variables',
-        options: ['Commas','Curly Brackets','Quotes','Parenthesis',],
+        options: ['Commas', 'Curly Brackets', 'Quotes', 'Parenthesis',],
         answer: 'Curly Brackets',
     },
     {
         question: 'A very useful tool used during development and debugging for prinitng content to the debugger',
-        options: ['Javascript','Terminal/Bash','For loops','Console.log',],
+        options: ['Javascript', 'Terminal/Bash', 'For loops', 'Console.log',],
         answer: 'For Loops',
     },
 ]
 
 startButton.addEventListener('click', startQuiz)
 
+// starts quiz
 function startQuiz() {
+    startButton 
     gameOver = false;
-    questionIndex = 0;
-    startButton.style.display = 'none'
-}
-
-
-
-function selectOption() {
-
-}
-
-nextButton.addEventListener('click', nextQuestion)
-
-function nextQuestion() {
-    questionArr ++;
-    if (questionArr >= questionsArr.options.length){
-        endGame();
-    } else {
-        showQuestions(questionArr);
-    }
- 
+    startButton.style.display = 'none';
+    showQuestions(questionsArr);
     return;
-}
-
-function endGame() {
-    gameEnded = true;
 }
 
 function showQuestions(currentQuestions) {
@@ -65,7 +45,29 @@ function showQuestions(currentQuestions) {
     createAnswer(currentQuestions);
     return;
 }
-    
+
+function createAnswer(currentQuestions) {
+    answerButtonList.innerHTML = '';
+}
+
+
+nextButton.addEventListener('click', nextQuestion)
+
+function nextQuestion() {
+    if (questionsArr >= questionsArr.options.length) {
+        endGame();
+    } else {
+        showQuestions(questionsArr);
+    }
+
+    return;
+}
+
+function endGame() {
+    gameOver = true;
+}
+
+
 
 
 
