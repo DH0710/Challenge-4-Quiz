d5a765648f8e6914b72f711c4e3cf613828350c3
blur <damienhebert710@gmail.com>
2022-05-17 19:54:35 -0500

    added first question and added previous button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,17 @@
 const startButton = document.querySelector("start-btn")
 const nextButton = document.querySelector("next-btn")
+const prevButton = document.querySelector("prev-btn")
+
 
 const questions = [
     {
-        question: ''
-        answers: 
+        question: 'Arrays in Javascript can be used to store:',
+        answers: [
+            { text: 'Booleans', correct: false }, 
+            { text: 'Numbers and Strings', correct: false },
+            { text: 'Other Arrays', correct: false },
+            { text: 'All of the above', correct: true },
+        ]
     }
 ]
 
